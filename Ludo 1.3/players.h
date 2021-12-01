3dfc06c0506bc99e6d9d80fa0b29ea1b99f77a3b
mateushzet <72745420+mateushzet@users.noreply.github.com>
2021-12-01 13:22:46 +0100

    Add files via upload

---
+++ Ludo 1.3/players.h
@@ -0,0 +1,26 @@
+#ifndef players_h
+#define players_h
+class Pawn
+{
+public:
+	int position;
+	bool inBase;
+	bool inFinal;
+	bool ended;
+	Pawn();
+};
+
+class Player
+{
+public:
+	int id;
+	int inBase;
+	int inEnded;
+	int meta;
+	int start;
+	char final_fields[5];
+	Pawn* pawns[4];
+	Player(int m, int s, int id);
+	~Player();
+};
+#endif
